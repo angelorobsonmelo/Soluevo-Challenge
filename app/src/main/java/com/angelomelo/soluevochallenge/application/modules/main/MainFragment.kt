682d1c3972db8748e1d68d8492f0d56a5112e5a4
angelomelo <angelorobsonmelo@gmail.com>
2019-03-27 22:13:49 -0300

    implemented search view

--- app/src/main/java/com/angelomelo/soluevochallenge/application/modules/main/MainFragment.kt
+++ app/src/main/java/com/angelomelo/soluevochallenge/application/modules/main/MainFragment.kt
@@ -1,13 +1,17 @@
 package com.angelomelo.soluevochallenge.application.modules.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.InputType
+import android.view.*
 import android.view.animation.OvershootInterpolator
+import android.view.inputmethod.EditorInfo
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil.inflate
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.angelomelo.soluevochallenge.R
 import com.angelomelo.soluevochallenge.application.modules.main.adapter.ContractAdapter
 import com.angelomelo.soluevochallenge.application.utils.FragmentBase
@@ -15,13 +19,13 @@ import com.angelomelo.soluevochallenge.databinding.MainFragmentBinding
 import com.angelomelo.soluevochallenge.domain.Contract
 import com.google.android.material.snackbar.Snackbar
 import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainFragment : FragmentBase() {
 
     private lateinit var binding: MainFragmentBinding
     private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: ContractAdapter
 
     companion object {
         fun newInstance() = MainFragment()
@@ -31,7 +35,7 @@ class MainFragment : FragmentBase() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding  = inflate(inflater, R.layout.main_fragment, container, false)
+        binding = inflate(inflater, R.layout.main_fragment, container, false)
         binding.lifecycleOwner = this
 
         return binding.root
@@ -41,7 +45,7 @@ class MainFragment : FragmentBase() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         binding.viewModel = viewModel
-
+        setHasOptionsMenu(true)
         setupElements()
 
         Snackbar.make(view?.rootView!!, "Seja bem vindo", Snackbar.LENGTH_LONG)
@@ -49,8 +53,21 @@ class MainFragment : FragmentBase() {
     }
 
     private fun setupElements() {
+        val toolbar = activity?.tollbarContracts as Toolbar
+        setSupportActionBar(toolbar)
+
         setupRecyclerView()
         setupObserverOnSuccess()
+    }
+
+    private fun setSupportActionBar(toolbar: Toolbar) {
+        val appCompatActivity = activity as AppCompatActivity?
+
+        appCompatActivity?.setSupportActionBar(toolbar)
+        appCompatActivity?.supportActionBar?.setDisplayShowTitleEnabled(true)
+        appCompatActivity?.supportActionBar?.title = "Contracts"
+
+//        toolbar.toolbar_title.text = getString(R.string.event_detail_title)
     }
 
     private fun setupRecyclerView() {
@@ -67,12 +84,37 @@ class MainFragment : FragmentBase() {
     }
 
     private fun setupAdapter(contracts: List<Contract>) {
-        val adapter = ContractAdapter(contracts)
+        adapter = ContractAdapter(contracts as MutableList<Contract>)
         binding.contractsRecyclerView.adapter = ScaleInAnimationAdapter(adapter).apply {
             setFirstOnly(false)
             setDuration(500)
             setInterpolator(OvershootInterpolator(.5f))
         }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_filter, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.imeOptions = EditorInfo.IME_ACTION_DONE
+        searchView.inputType  = InputType.TYPE_CLASS_NUMBER
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                adapter.filter.filter(newText)
+                return false
+            }
+        })
+
+        super.onCreateOptionsMenu(menu, inflater)
+
     }
 
 }
--- app/src/main/java/com/angelomelo/soluevochallenge/application/modules/main/adapter/ContractAdapter.kt
+++ app/src/main/java/com/angelomelo/soluevochallenge/application/modules/main/adapter/ContractAdapter.kt
@@ -2,12 +2,18 @@ package com.angelomelo.soluevochallenge.application.modules.main.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.angelomelo.soluevochallenge.R
 import com.angelomelo.soluevochallenge.application.modules.main.viewHolder.ContractViewHolder
 import com.angelomelo.soluevochallenge.domain.Contract
 
-class ContractAdapter(private val contracts: List<Contract>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+class ContractAdapter(private val contracts: MutableList<Contract>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
+    Filterable {
+
+    private var exampleListFull: List<Contract> = ArrayList(contracts)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return ContractViewHolder(
@@ -23,5 +29,38 @@ class ContractAdapter(private val contracts: List<Contract>) : RecyclerView.Adap
     }
 
     override fun getItemCount() = contracts.size
+
+    override fun getFilter(): Filter {
+        return exampleFilter
+    }
+
+    private val exampleFilter = object : Filter() {
+        override fun performFiltering(constraint: CharSequence?): Filter.FilterResults {
+            val filteredList = ArrayList<Contract>()
+
+            if (constraint == null || constraint.isEmpty()) {
+                filteredList.addAll(exampleListFull)
+            } else {
+                val filterPattern = constraint.toString().toLowerCase().trim { it <= ' ' }
+
+                for (item in exampleListFull) {
+                    if (item.code.toString().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(item)
+                    }
+                }
+            }
+
+            val results = Filter.FilterResults()
+            results.values = filteredList
+
+            return results
+        }
+
+        override fun publishResults(constraint: CharSequence, results: Filter.FilterResults) {
+            contracts.clear()
+            contracts.addAll(results.values as List<Contract>)
+            notifyDataSetChanged()
+        }
+    }
 
 }
