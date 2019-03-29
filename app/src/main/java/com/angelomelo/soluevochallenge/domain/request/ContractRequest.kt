01490ceec0d0906cb98f5ca23e19e3261eceb4f2
angelomelo <angelorobsonmelo@gmail.com>
2019-03-28 23:39:53 -0300

    Implementing contract form

--- app/src/main/java/com/angelomelo/soluevochallenge/domain/request/ContractRequest.kt
+++ app/src/main/java/com/angelomelo/soluevochallenge/domain/request/ContractRequest.kt
@@ -64,9 +64,12 @@ data class Credor(
 data class Contratos(
     @SerializedName("quantidade_meses")
     var quantidadeMeses: Int = 0,
+    var quantidadeMesesField: String = "",
     var comissao: Int = 0,
     @SerializedName("taxa_mora")
     var taxaMora: Int = 0,
+    @Expose
+    var taxaMoraField: String = "",
     @SerializedName("valor_taxa_mora")
     var valorTaxaMora: Int = 0,
     @SerializedName("valor_taxa_de_contrato")
