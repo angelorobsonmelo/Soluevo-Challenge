bd1b04b469aa87c1aa33dc6093f32957a358a8c8
angelomelo <angelorobsonmelo@gmail.com>
2019-03-31 17:20:58 -0300

    set toolbar action go to contracts screens and forms

--- app/src/main/java/com/angelomelo/soluevochallenge/application/modules/savecontract/StateProgressBarBaseActivity.kt
+++ app/src/main/java/com/angelomelo/soluevochallenge/application/modules/savecontract/StateProgressBarBaseActivity.kt
@@ -1,5 +1,8 @@
 package com.angelomelo.soluevochallenge.application.modules.savecontract
 
+import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
@@ -7,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.angelomelo.soluevochallenge.R
 import com.angelomelo.soluevochallenge.application.SoluevoChallengeApplication
+import com.angelomelo.soluevochallenge.application.modules.main.MainActivity
 import com.kofigyan.stateprogressbar.StateProgressBar
 import kotlinx.android.synthetic.main.state_progress_bar_base.*
 
@@ -17,7 +21,6 @@ abstract class StateProgressBarBaseActivity : AppCompatActivity(), View.OnClickL
      private val creditor = SoluevoChallengeApplication.instance.resources.getString(R.string.creditor)
      private val contract = SoluevoChallengeApplication.instance.resources.getString(R.string.contract)
      private val attachments = SoluevoChallengeApplication.instance.resources.getString(R.string.attachments)
-
      private var descriptionData = arrayOf(personal, vehicle, creditor, contract, attachments)
 
      lateinit var nextBtn: Button
@@ -29,6 +32,20 @@ abstract class StateProgressBarBaseActivity : AppCompatActivity(), View.OnClickL
         setSupportActionBar(toolbar as Toolbar)
         supportActionBar?.title = title
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_forms, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        goToContractsCreen()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun goToContractsCreen() {
+        startActivity(Intent(this, MainActivity::class.java))
     }
 
     override fun onSupportNavigateUp(): Boolean {
